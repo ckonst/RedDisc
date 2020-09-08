ee5eef977e79c02eae2947a3a4d9c85e0211ca52
ckonst <christian.konstantinov98@gmail.com>
2020-09-08 19:39:12 -0400

    added on_ready event and quit command and client secrets into a .env file

--- main.py
+++ main.py
@@ -2,12 +2,32 @@ import discord
 from discord import Client
 from discord.ext.commands import Bot
 import praw
+from dotenv import load_dotenv
+import os
+
+# comment out these two lines if you are not using spyder
+import nest_asyncio
+nest_asyncio.apply()
 
 BOT_PREFIX = ('!')
 client = Bot(command_prefix=BOT_PREFIX)
-reddit = praw.Reddit(client_id="oBMX1JClXgCF0A",
-                            client_secret="2V0JrFT97T1iOzMVrcjqNstg1LM",
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+SECRET = os.getenv('CLIENT_SECRET') + 'M'
+ID = os.getenv('CLIENT_ID')
+print(SECRET)
+print(ID)
+
+reddit = praw.Reddit(client_id=ID,
+                            client_secret=SECRET,
                             user_agent="EpicBot for Reddit")
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
 @client.command(name='hey',
                 brief='The bot says hi',
@@ -24,6 +44,21 @@ async def title(ctx, *args):
     if not args:
         await ctx.channel.send('Please specify subreddit')
     for submission in reddit.subreddit(args[0]).hot(limit=5):
+        print(submission)
         await ctx.channel.send(submission.title)
 
-client.run("NzUxNjgyNzA0NjcxNjM3NTE0.X1MpEg.Wk1noo-DKjXcGLWyyxc4GEd0Gjs")
+@client.command(name = 'quit',
+				brief = 'closes the bot',
+				aliases = ['q', 'e', 'exit'])
+async def quit(ctx):
+    await ctx.send('*exiting*')
+    await client.logout()
+
+if __name__ == '__main__':
+	try:
+		client.run(TOKEN)
+	except KeyboardInterrupt:
+		print('exiting\n')
+		exit(0)
+	except:
+		pass
