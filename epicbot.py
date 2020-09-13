e40d5dcec35a62aa5d8abfff06b8df68c9dd6952
parmati94 <53498049+parmati94@users.noreply.github.com>
2020-09-12 22:06:52 -0400

    Add files via upload
    Added embeds and all post categorizations for subreddits

--- epicbot.py
+++ epicbot.py
@@ -6,8 +6,8 @@ from dotenv import load_dotenv
 import os
 
 # comment out these two lines if you are not using spyder
-# import nest_asyncio
-# nest_asyncio.apply()
+#import nest_asyncio
+#nest_asyncio.apply()
 
 BOT_PREFIX = ('!')
 client = Bot(command_prefix=BOT_PREFIX)
@@ -16,76 +16,90 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 SECRET = os.getenv('CLIENT_SECRET')
 ID = os.getenv('CLIENT_ID')
+aliases = ['hot{}'.format(i+1) for i in range(10)]+['top{}'.format(i+1) for i in range(10)]+['new{}'.format(i+1) for i in range(10)]+ ['rising{}'.format(i+1) for i in range(10)]
 topx = ['top{}'.format(i+1) for i in range(10)]
-base_url = 'https://www.reddit.com'
+baseURL = 'https://www.reddit.com'
+
+
 reddit = praw.Reddit(client_id=ID,
-                     client_secret=SECRET,
-                     user_agent='EpicBot for Reddit')
-
-
+                            client_secret=SECRET,
+                            user_agent="EpicBot for Reddit")
 @client.event
 async def on_ready():
-    """
-    Print messages to indicate a successful login.
-
-    Returns
-    -------
-    None.
-
-    """
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
 
+@client.command(name='hey',
+                brief='The bot says hi',
+                aliases = ['hi','greetings','hello'])
+async def hey(ctx):
+    # we do not want the bot to reply to itself
+    if ctx.message.author.id == client.user.id:
+        return
+    await ctx.send('hey {0.author.mention}'.format(ctx.message))
 
-@client.command(name='top',
-                brief='Post given number of posts from a subreddit.',
-                aliases=topx)
-async def top(ctx, *args):
-    """
-    Post the top x posts from a given subreddit to the channel.
-
-    Parameters
-    ----------
-    ctx : discord.ext.commands.Context
-        The context in which the command is being invoked under.
-    *args : list
-        The list of transformed arguments that were passed into the command.
-
-    Returns
-    -------
-    None.
-
-    """
+@client.command(name='Post',
+                brief='Posts given number of posts from a subreddit.',
+                aliases = aliases)
+async def hot(ctx, *args):
     if not args:
         await ctx.channel.send('Please specify subreddit')
         return
-    if ctx.invoked_with not in topx:
-        await ctx.channel.send('Invalid command.\
-                               Use !help for a list of valid commands.')
+    if ctx.invoked_with not in aliases:
+        await ctx.channel.send('Invalid command.  Use !help for a list of valid commands.')
         return
     lim = ''.join(c for c in ctx.invoked_with if c.isdigit())
     if not lim:
         lim = 5
     lim = int(lim)
 
-    for submission in reddit.subreddit(args[0]).hot(limit=lim):
-        embed = discord.Embed(title=submission.title, color=0xff5700)
-        embed.set_author(name='u/'+submission.author.name,
-                         icon_url=submission.author.icon_img)
+
+
+    command = ''.join(c for c in ctx.invoked_with if not c.isdigit()).lower()
+
+
+    post_type = None
+
+
+    if command == 'hot':
+        post_type = reddit.subreddit(args[0]).hot(limit=lim)
+    elif command == 'top':
+        post_type = reddit.subreddit(args[0]).top(limit=lim)
+    elif command == 'new':
+        post_type = reddit.subreddit(args[0]).rising(limit=lim)
+    elif command == 'rising':
+        post_type = reddit.subreddit(args[0]).new(limit=lim)
+
+    for submission in post_type:
+        embedVar = discord.Embed(title = submission.title, color = 0xff5700)
+        embedVar.set_author(name="u/"+submission.author.name, icon_url= submission.author.icon_img)
         if not submission.is_self:
-            embed.set_image(url=submission.url)
-        embed.add_field(name='URL:', value=base_url + submission.permalink)
-        embed.add_field(name='Upvotes:', value=submission.score)
-        embed.set_thumbnail(url='https://i.imgur.com/5uefD9U.png')
-        await ctx.channel.send(embed=embed)
+            embedVar.set_image(url= submission.url)
+        embedVar.add_field(name="URL:", value=baseURL + submission.permalink)
+        embedVar.add_field(name="Upvotes:", value=submission.score)
+        embedVar.set_thumbnail(
+            url='https://i.imgur.com/5uefD9U.png',
+                  )
+        await ctx.channel.send(embed=embedVar)
+
+
+
+
+#await ctx.channel.send(submission.title)
+#await ctx.channel.send("COMMENT:" + submission.comments[1].body)
+#await ctx.channel.send("UPVOTES: {}".format(submission.score))
+
+
+
+
 
 if __name__ == '__main__':
-    try:
-        client.run(TOKEN)
-    except KeyboardInterrupt:
-        print('exiting\n')
-        exit(0)
-    except:
-        pass
+	try:
+		client.run(TOKEN)
+	except KeyboardInterrupt:
+		print('exiting\n')
+		exit(0)
+	except:
+		pass
