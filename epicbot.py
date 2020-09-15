9eb111daa22a7494f1ad9c39171bfa37a2e4944c
Phil <philtm@yahoo.com>
2020-09-15 18:47:26 -0400

    Update epicbot.py

--- epicbot.py
+++ epicbot.py
@@ -6,8 +6,13 @@ from dotenv import load_dotenv
 import os
 
 # comment out these two lines if you are not using spyder
-#import nest_asyncio
-#nest_asyncio.apply()
+<<<<<<< HEAD
+import nest_asyncio
+nest_asyncio.apply()
+=======
+# import nest_asyncio
+# nest_asyncio.apply()
+>>>>>>> Christian
 
 BOT_PREFIX = ('!')
 client = Bot(command_prefix=BOT_PREFIX)
@@ -16,6 +21,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 SECRET = os.getenv('CLIENT_SECRET')
 ID = os.getenv('CLIENT_ID')
+<<<<<<< HEAD
 aliases = ['hot{}'.format(i+1) for i in range(10)]+['top{}'.format(i+1) for i in range(10)]+['new{}'.format(i+1) for i in range(10)]+ ['rising{}'.format(i+1) for i in range(10)]
 topx = ['top{}'.format(i+1) for i in range(10)]
 baseURL = 'https://www.reddit.com'
@@ -26,11 +32,31 @@ reddit = praw.Reddit(client_id=ID,
                             user_agent="EpicBot for Reddit")
 @client.event
 async def on_ready():
+=======
+topx = ['top{}'.format(i+1) for i in range(10)]
+base_url = 'https://www.reddit.com'
+reddit = praw.Reddit(client_id=ID,
+                     client_secret=SECRET,
+                     user_agent='EpicBot for Reddit')
+
+
+@client.event
+async def on_ready():
+    """
+    Print messages to indicate a successful login.
+
+    Returns
+    -------
+    None.
+
+    """
+>>>>>>> Christian
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
 
+<<<<<<< HEAD
 @client.command(name='hey',
                 brief='The bot says hi',
                 aliases = ['hi','greetings','hello'])
@@ -49,12 +75,41 @@ async def hot(ctx, *args):
         return
     if ctx.invoked_with not in aliases:
         await ctx.channel.send('Invalid command.  Use !help for a list of valid commands.')
+=======
+
+@client.command(name='top',
+                brief='Post given number of posts from a subreddit.',
+                aliases=topx)
+async def top(ctx, *args):
+    """
+    Post the top x posts from a given subreddit to the channel.
+
+    Parameters
+    ----------
+    ctx : discord.ext.commands.Context
+        The context in which the command is being invoked under.
+    *args : list
+        The list of transformed arguments that were passed into the command.
+
+    Returns
+    -------
+    None.
+
+    """
+    if not args:
+        await ctx.channel.send('Please specify subreddit')
+        return
+    if ctx.invoked_with not in topx:
+        await ctx.channel.send('Invalid command.\
+                               Use !help for a list of valid commands.')
+>>>>>>> Christian
         return
     lim = ''.join(c for c in ctx.invoked_with if c.isdigit())
     if not lim:
         lim = 5
     lim = int(lim)
 
+<<<<<<< HEAD
 
 
     command = ''.join(c for c in ctx.invoked_with if not c.isdigit()).lower()
@@ -105,3 +160,24 @@ if __name__ == '__main__':
 		exit(0)
 	except:
 		pass
+=======
+    for submission in reddit.subreddit(args[0]).hot(limit=lim):
+        embed = discord.Embed(title=submission.title, color=0xff5700)
+        embed.set_author(name='u/'+submission.author.name,
+                         icon_url=submission.author.icon_img)
+        if not submission.is_self:
+            embed.set_image(url=submission.url)
+        embed.add_field(name='URL:', value=base_url + submission.permalink)
+        embed.add_field(name='Upvotes:', value=submission.score)
+        embed.set_thumbnail(url='https://i.imgur.com/5uefD9U.png')
+        await ctx.channel.send(embed=embed)
+
+if __name__ == '__main__':
+    try:
+        client.run(TOKEN)
+    except KeyboardInterrupt:
+        print('exiting\n')
+        exit(0)
+    except:
+        pass
+>>>>>>> Christian
