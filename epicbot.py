40dcbcb01784e4be0db49d24e16afa3a8fc015be
ckonst <55851360+ckonst@users.noreply.github.com>
2020-09-22 22:23:19 -0400

    Add files via upload

--- epicbot.py
+++ epicbot.py
@@ -1,13 +1,18 @@
 import discord
-from discord import Client
 from discord.ext.commands import Bot
 import praw
 from dotenv import load_dotenv
 import os
+from prawcore import NotFound
 
 # comment out these two lines if you are not using spyder
-# import nest_asyncio
-# nest_asyncio.apply()
+import nest_asyncio
+nest_asyncio.apply()
+
+"""
+TODO:
+    use_snake_case_you_inbreds
+"""
 
 BOT_PREFIX = ('!')
 client = Bot(command_prefix=BOT_PREFIX)
@@ -16,13 +21,14 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 SECRET = os.getenv('CLIENT_SECRET')
 ID = os.getenv('CLIENT_ID')
-topx = ['top{}'.format(i+1) for i in range(10)]
+sort = ['hot', 'top', 'new', 'rising']
+aliases = [f'{s}{j}' for j in range(1, 11) for s in sort]
 base_url = 'https://www.reddit.com'
+
+
 reddit = praw.Reddit(client_id=ID,
-                     client_secret=SECRET,
-                     user_agent='EpicBot for Reddit')
-
-
+                            client_secret=SECRET,
+                            user_agent="EpicBot for Reddit")
 @client.event
 async def on_ready():
     """
@@ -38,42 +44,56 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-
-@client.command(name='top',
-                brief='Post given number of posts from a subreddit.',
-                aliases=topx)
-async def top(ctx, *args):
+@client.command(name='hot',
+                brief='Posts given number of posts from a subreddit.',
+                aliases = aliases)
+async def hot(ctx, *args):
     """
-    Post the top x posts from a given subreddit to the channel.
+    Returns a given number of posts, up to 10, sorted by the alias that this was invoked with.
 
     Parameters
     ----------
     ctx : discord.ext.commands.Context
-        The context in which the command is being invoked under.
+        The context in which this command was called.
     *args : list
-        The list of transformed arguments that were passed into the command.
+        The arguments that the user passed through.
+        If the user does not specify a subreddit, or the given subreddit does not exist, send an error message.
 
     Returns
     -------
     None.
 
     """
+
+    #check for invocation errors
+    command = ctx.invoked_with
     if not args:
-        await ctx.channel.send('Please specify subreddit')
+        await ctx.channel.send(f'Please specify subreddit! Correct usage is: !{command} [subreddit]')
         return
-    if ctx.invoked_with not in topx:
-        await ctx.channel.send('Invalid command.\
-                               Use !help for a list of valid commands.')
+    sub = args[0]
+    if not sub_exists(sub):
+        await ctx.channel.send(f'Specified subreddit {sub} does not exist.')
         return
-    lim = ''.join(c for c in ctx.invoked_with if c.isdigit())
+    if command not in aliases:
+        await ctx.channel.send('Invalid command. Use !help for a list of valid commands.')
+        return
+
+    #extract sort method and retrieval count from command
+    sort_by = ''.join([s for s in sort if s in command]).lower()
+    lim = command.replace(sort_by, '')
     if not lim:
         lim = 5
     lim = int(lim)
+    results = getattr(reddit.subreddit(sub), sort_by)(limit=lim)
 
-    for submission in reddit.subreddit(args[0]).hot(limit=lim):
-        embed = discord.Embed(title=submission.title, color=0xff5700)
-        embed.set_author(name='u/'+submission.author.name,
-                         icon_url=submission.author.icon_img)
+    #make requests for each submission and format them into discord embeds.
+    for submission in results:
+        abridged_title = (submission.title[:250])
+        if len(submission.title) > 250:
+            abridged_title += '...'
+        embed = discord.Embed(title=abridged_title, color=0xff5700)
+        if user_exists(submission.author.name):
+            embed.set_author(name=f'u/{submission.author.name}', icon_url=submission.author.icon_img)
         if not submission.is_self:
             embed.set_image(url=submission.url)
         embed.add_field(name='URL:', value=base_url + submission.permalink)
@@ -81,11 +101,41 @@ async def top(ctx, *args):
         embed.set_thumbnail(url='https://i.imgur.com/5uefD9U.png')
         await ctx.channel.send(embed=embed)
 
-if __name__ == '__main__':
+def sub_exists(subreddit):
+    """
+    Return whether a given subreddit exists.
+
+    Parameters
+    ----------
+    subreddit : praw.models.Subreddit
+        The subreddit object to check.
+
+    Returns
+    -------
+    exists : boolean
+        whether or not the subreddit exists.
+
+    """
+    exists = True
     try:
-        client.run(TOKEN)
-    except KeyboardInterrupt:
-        print('exiting\n')
-        exit(0)
-    except:
-        pass
+        reddit.subreddits.search_by_name(subreddit, exact=True)
+    except NotFound:
+        exists = False
+    return exists
+
+def user_exists(user):
+    try:
+        if getattr(reddit.redditor(user), 'is_suspended', False):
+            return False
+    except NotFound:
+        return False
+    return True
+
+if __name__ == '__main__':
+	try:
+		client.run(TOKEN)
+	except KeyboardInterrupt:
+		print('exiting\n')
+		exit(0)
+	except:
+		pass
