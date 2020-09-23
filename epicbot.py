4a3f9fe0a759100740d70585a0a91be9d97e346b
ckonst <christian.konstantinov98@gmail.com>
2020-09-22 21:42:21 -0400

    updated some code and variables to be more P Y T H O N I C and added some documentation

--- epicbot.py
+++ epicbot.py
@@ -1,5 +1,4 @@
 import discord
-from discord import Client
 from discord.ext.commands import Bot
 import praw
 from dotenv import load_dotenv
@@ -7,8 +6,13 @@ import os
 from prawcore import NotFound
 
 # comment out these two lines if you are not using spyder
-#import nest_asyncio
-#nest_asyncio.apply()
+import nest_asyncio
+nest_asyncio.apply()
+
+"""
+TODO:
+    use_snake_case_you_inbreds
+"""
 
 BOT_PREFIX = ('!')
 client = Bot(command_prefix=BOT_PREFIX)
@@ -17,12 +21,9 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 SECRET = os.getenv('CLIENT_SECRET')
 ID = os.getenv('CLIENT_ID')
-aliases = ['hot{}'.format(i+1) for i in range(10)]+\
-          ['top{}'.format(i+1) for i in range(10)]+\
-          ['new{}'.format(i+1) for i in range(10)]+ \
-          ['rising{}'.format(i+1) for i in range(10)]
-topx = ['top{}'.format(i+1) for i in range(10)]
-baseURL = 'https://www.reddit.com'
+sort = ['hot', 'top', 'new', 'rising']
+aliases = [f'{s}{j}' for j in range(1, 11) for s in sort]
+base_url = 'https://www.reddit.com'
 
 
 reddit = praw.Reddit(client_id=ID,
@@ -30,71 +31,91 @@ reddit = praw.Reddit(client_id=ID,
                             user_agent="EpicBot for Reddit")
 @client.event
 async def on_ready():
+    """
+    Print messages to indicate a successful login.
+
+    Returns
+    -------
+    None.
+
+    """
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
 
-@client.command(name='hey',
-                brief='The bot says hi',
-                aliases = ['hi','greetings','hello'])
-async def hey(ctx):
-    # we do not want the bot to reply to itself
-    if ctx.message.author.id == client.user.id:
-        return
-    await ctx.send('hey {0.author.mention}'.format(ctx.message))
-
-@client.command(name='Post',
+@client.command(name='hot',
                 brief='Posts given number of posts from a subreddit.',
                 aliases = aliases)
 async def hot(ctx, *args):
+    """
+    Returns a given number of posts, up to 10, sorted by the alias that this was invoked with.
+
+    Parameters
+    ----------
+    ctx : discord.ext.commands.Context
+        The context in which this command was called.
+    *args : list
+        The arguments that the user passed through.
+        If the user does not specify a subreddit, or the given subreddit does not exist, send an error message.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    #check for invocation errors
+    command = ctx.invoked_with
     if not args:
-        await ctx.channel.send('Please specify subreddit!  Correct usage is: !'+ctx.invoked_with + ' [subreddit]')
+        await ctx.channel.send(f'Please specify subreddit! Correct usage is: !{command} [subreddit]')
         return
-    if not sub_exists(args[0]):
-        await ctx.channel.send('Specified subreddit "' + args[0] + '" does not exist.')
+    sub = args[0]
+    if not sub_exists(sub):
+        await ctx.channel.send(f'Specified subreddit {sub} does not exist.')
         return
-    if ctx.invoked_with not in aliases:
-        await ctx.channel.send('Invalid command.  Use !help for a list of valid commands.')
+    if command not in aliases:
+        await ctx.channel.send('Invalid command. Use !help for a list of valid commands.')
         return
-    lim = ''.join(c for c in ctx.invoked_with if c.isdigit())
+
+    #extract sort method and retrieval count from command
+    sort_by = ''.join([s for s in sort if s in command]).lower()
+    lim = command.replace(sort_by, '')
     if not lim:
         lim = 5
     lim = int(lim)
+    results = getattr(reddit.subreddit(sub), sort_by)(limit=lim)
 
-
-
-    command = ''.join(c for c in ctx.invoked_with if not c.isdigit()).lower()
-
-
-    post_type = None
-
-
-    if command == 'hot':
-        post_type = reddit.subreddit(args[0]).hot(limit=lim)
-    elif command == 'top':
-        post_type = reddit.subreddit(args[0]).top(limit=lim)
-    elif command == 'new':
-        post_type = reddit.subreddit(args[0]).rising(limit=lim)
-    elif command == 'rising':
-        post_type = reddit.subreddit(args[0]).new(limit=lim)
-
-    for submission in post_type:
-        abridged_title = (submission.title[:250] + '...') if len(submission.title) > 250 else submission.title
-        embedVar = discord.Embed(title = abridged_title, color = 0xff5700)
-        #if user_exists(submission.author.name):
-        embedVar.set_author(name="u/"+submission.author.name, icon_url= submission.author.icon_img)
+    #make requests for each submission and format them into discord embeds.
+    for submission in results:
+        abridged_title = (submission.title[:250])
+        if len(submission.title) > 250:
+            abridged_title += '...'
+        embed = discord.Embed(title=abridged_title, color=0xff5700)
+        if user_exists(submission.author.name):
+            embed.set_author(name=f'u/{submission.author.name}', icon_url=submission.author.icon_img)
         if not submission.is_self:
-            embedVar.set_image(url= submission.url)
-        embedVar.add_field(name="URL:", value=baseURL + submission.permalink)
-        embedVar.add_field(name="Upvotes:", value=submission.score)
-        embedVar.set_thumbnail(
-            url='https://i.imgur.com/5uefD9U.png',
-                  )
-        await ctx.channel.send(embed=embedVar)
-
+            embed.set_image(url=submission.url)
+        embed.add_field(name='URL:', value=base_url + submission.permalink)
+        embed.add_field(name='Upvotes:', value=submission.score)
+        embed.set_thumbnail(url='https://i.imgur.com/5uefD9U.png')
+        await ctx.channel.send(embed=embed)
 
 def sub_exists(subreddit):
+    """
+    Return whether a given subreddit exists.
+
+    Parameters
+    ----------
+    subreddit : praw.models.Subreddit
+        The subreddit object to check.
+
+    Returns
+    -------
+    exists : boolean
+        whether or not the subreddit exists.
+
+    """
     exists = True
     try:
         reddit.subreddits.search_by_name(subreddit, exact=True)
@@ -102,21 +123,13 @@ def sub_exists(subreddit):
         exists = False
     return exists
 
-##def user_exists(user):
-    #exists = True
-   # try:
-   #     reddit.redditor(user)
-   # except NotFound:
-  #      exists = False
- #  return exists
-
-#await ctx.channel.send(submission.title)
-#await ctx.channel.send("COMMENT:" + submission.comments[1].body)
-#await ctx.channel.send("UPVOTES: {}".format(submission.score))
-
-
-
-
+def user_exists(user):
+    try:
+        if getattr(reddit.redditor(user), 'is_suspended', False):
+            return False
+    except NotFound:
+        return False
+    return True
 
 if __name__ == '__main__':
 	try:
