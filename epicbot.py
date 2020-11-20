b2241c61eb1318e20a1484c9bb84deebc729b14f
parmati94 <53498049+parmati94@users.noreply.github.com>
2020-11-19 22:01:58 -0500

    Paul (#10)
    * Fixed comment embeds to only include "..." when length of the comment is greater than 100 chars
    
    * Fixed comment embeds to only include "..." when length of the comment is greater than 100 chars
    
    * Modified url_morph to check for multiple image extensions with imgur url
    
    * Delete Project_Default.xml
    
    * Delete profiles_settings.xml
    
    * Delete epicbot.iml
    
    * Delete misc.xml
    
    * Delete modules.xml
    
    * Delete vcs.xml
    
    * Delete workspace.xml
    
    Co-authored-by: ckonst <55851360+ckonst@users.noreply.github.com>

--- epicbot.py
+++ epicbot.py
@@ -42,6 +42,7 @@ aliases = [f'{s}{j}' for j in range(1, 11) for s in sort]
 emojis = ['📃','📄', '👤', '💬' ]
 search_sorts = ['relevance', 'top', 'new', 'comments']
 filters = ['all', 'hour', 'day', 'week', 'month', 'year']
+extensions = ['gif', 'jpg', 'png']
 base_url = 'https://www.reddit.com'
 title_lim = 250
 body_lim = 1020
@@ -484,7 +485,6 @@ def to_query_string(args):
 
 def is_image(url):
     """Return whether or not the given url links to an image."""
-    extensions = ['gif', 'jpg', 'png']
     return any(e in url for e in extensions)
 
 def url_morph(url):
@@ -501,7 +501,7 @@ def url_morph(url):
     """
     if 'gifv' in url:
         url = url[:-1]
-    if 'imgur'in url and 'jpg' not in url:
+    if 'imgur'in url and 'jpg' not in url and 'gif' not in url and 'png' not in url:
         url += '.jpg'
     return url
 
@@ -556,4 +556,3 @@ if __name__ == '__main__':
 		exit(0)
 	except:
 		pass
-  
